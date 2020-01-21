07ede16a415800d1320b65521f1a1fd877f6c5bb
LBlend <24893890+LBlend@users.noreply.github.com>
2020-01-21 21:36:24 +0100

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# DNB-Transaction-Calculator
-A simple script that sorts your transactions by keyword and calculates the total amount
+# DNB Transaction Calculator
+A simple script that sorts your transactions by keyword and calculates the total amount. This makes you able to calculate how much you've spent in/on a specific store/service.
+
+## Usage
+
+1. Clone the repo
+    `git clone https://github.com/LBlend/DNB-Transaction-Calculator.git`
+
+2. Install [python](https://www.python.org/downloads/)
+    *Minimum requirement is Python 3.6*
+
+3. Install xlrd
+    `python -m pip install xlrd`
+
+4. Make a new folder called `input`
+
+5. Download your transaction data
+
+6. Put the `.xls` files that you've downloaded into the `input` folder
+
+7. Run the script
+    `python calculator.py`
+
+## To-do list
+A list of features I want to add (*If I ever feel like expanding the functionality of this script*):
+ - [ ] GUI
+ - [ ] Support multiple banks
+ - [ ] Multi-language support
+ - [ ] Adding multiple sorting options
+
+## Why does this exist?
+I wanted to figure out how much money I'd spent on parking. I found it pretty tedious to enter all of my transactions manually into a calculator so I wrote a really simple script to do it for me. I figured I might aswell publish it because... why not? ¯\_(ツ)_/¯
+
+## Lisence
+This project is licensed under the MIT License - Please read [LICENSE](LICENSE) for details.
---
+++ calculator.py
@@ -0,0 +1,51 @@
+import xlrd
+from os import listdir
+from datetime import datetime
+
+
+def main():
+    sortword = input('Type the word that you want to sort the data by\n')
+    print_transactions = enter_transactions()
+    calculate(sortword, print_transactions)
+
+
+def enter_transactions():
+    """Gives the user the option to print transaction details"""
+
+    transcations = input('\n\nPrint all transactions?\ny/n\n')
+    if transcations.lower() == 'n':
+        return False
+    elif transcations.lower() == 'y':
+        return True
+    else:
+        print('\nINVALID INPUT\n\n')
+        return enter_transactions()
+
+
+def calculate(sortword: str, print_transactions: bool):
+    """Calculates the total amount"""
+
+    total = 0
+
+    if print_transactions is True:
+        print('\nDate | Transaction | Cost')
+
+    for i in listdir('input'):
+        excel_file = xlrd.open_workbook(filename=f'input/{i}')
+        excel_sheet = excel_file.sheet_by_index(0)
+        for row in range(excel_sheet.nrows):
+            date = excel_sheet.cell_value(row, 0)
+            name = excel_sheet.cell_value(row, 1)
+            cost = excel_sheet.cell_value(row, 3)
+            if sortword.lower() not in name.lower():
+                continue
+            if print_transactions is True:
+                date = datetime(*xlrd.xldate_as_tuple(date, excel_file.datemode)).strftime('%B %d %Y')
+                print(f'{date} | {name} | {cost}')
+
+            total += cost
+
+    print(f'\n\nTOTAL: {round(total, 2)}')
+
+
+main()  # Runs the script
